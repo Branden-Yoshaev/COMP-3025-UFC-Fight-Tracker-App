11653d974c373cd6930b0ceeeb4a9399b12df18e
Branden-Yoshaev <brandensam8@icloud.com>
2022-02-10 22:28:30 -0500

    Initial commit

---
+++ app/src/main/java/com/lh1137913/comp_3025_ufc_fight_tracker_app/Fighters1.kt
@@ -0,0 +1,11 @@
+package com.lh1137913.comp_3025_ufc_fight_tracker_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Fighters1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fighters1)
+    }
+}
