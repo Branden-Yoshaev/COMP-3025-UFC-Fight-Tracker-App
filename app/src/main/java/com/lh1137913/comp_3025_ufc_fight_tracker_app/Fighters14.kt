c5557dab59739e16bb2666b606812c73e2d32dce
Branden-Yoshaev <brandensam8@icloud.com>
2022-03-17 15:08:45 -0400

    Created info on fighters 1-8

---
+++ app/src/main/java/com/lh1137913/comp_3025_ufc_fight_tracker_app/Fighters14.kt
@@ -0,0 +1,4 @@
+package com.lh1137913.comp_3025_ufc_fight_tracker_app
+
+class Fighters14 {
+}
--- app/src/main/java/com/lh1137913/comp_3025_ufc_fight_tracker_app/Fighters15.java
+++
@@ -1,6 +0,0 @@
-package com.lh1137913.comp_3025_ufc_fight_tracker_app;
-
-import android.app.Activity;
-
-public class Fighters15 extends Activity {
-}
